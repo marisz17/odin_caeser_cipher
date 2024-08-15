ac9bf2505768f6409b85dcc36024d70639b2984e
marisz17 <117833301+marisz17@users.noreply.github.com>
2024-08-15 11:46:12 +0200

    Update rubocop compliant caesar_cipher.rb
    Checked with rubocop contains no offences.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,22 @@
-def caesar_cipher(string, shift_number)
-  def keep_same_case(character, shift_number)
-    def new_key(character, shift_number)
-      alphabet = Array("a".."z")
-      character_index = alphabet.index(character)
-      new_alphabet = alphabet.shift(shift_number).unshift(alphabet).flatten!
-      character_index ? new_alphabet[character_index] : character
-    end
-  
-    if character.ord >= 65 && character.ord < 91
-      return new_key(character.downcase,shift_number).upcase
-    else
-      return new_key(character, shift_number)
-    end
-  end
-  string.split("").map {|character| keep_same_case(character, shift_number)}.join("")
+# frozen_string_literal: true
+
+def new_key(character, shift_number)
+  alphabet = Array('a'..'z')
+  character_index = alphabet.index(character)
+  new_alphabet = alphabet.shift(shift_number).unshift(alphabet).flatten!
+  character_index ? new_alphabet[character_index] : character
 end
 
-p caesar_cipher("What a string!", 5)
+def keep_same_case(character, shift_number)
+  if character.ord >= 65 && character.ord < 91
+    new_key(character.downcase, shift_number).upcase
+  else
+    new_key(character, shift_number)
+  end
+end
+
+def caesar_cipher(string, shift_number)
+  string.split('').map { |character| keep_same_case(character, shift_number) }.join('')
+end
+
+p caesar_cipher('What a string!', 5)
